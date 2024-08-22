9d8a38d06352dfe49031e2aab88a4daff42f42a6
WhereIsLijah <114761023+WhereIsLijah@users.noreply.github.com>
2024-08-22 02:56:41 -0400

    Feature update: Donation Page Update

--- castle-and-care-frontend/src/pages/donation.tsx
+++ castle-and-care-frontend/src/pages/donation.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import '../styles/pages/donation.css';
+import DonateImage from "../images/donation/black-kids.jpg"
 
 const Donation: React.FC = () => {
     const [selectedFrequency, setSelectedFrequency] = useState<string>('One Time');
@@ -14,69 +15,75 @@ const Donation: React.FC = () => {
     };
 
     return (
-        <div className="container">
-            <div className="donation-text">
-                <h1>DONATE ONLINE</h1>
-                <p>
-                    Your donations helps us create lasting change by providing education,
-                    supporting healthcare, empowering communities, and making a real
-                    difference in the lives of those we serve.
-                </p>
+        <>
+            <div className="donation-image">
+                <img src={DonateImage} alt="Black girls pose for a picture smiling"/>
             </div>
-            <p>Frequency</p>
-            <div className="frequency-buttons">
-                <button
-                    className={selectedFrequency === 'One Time' ? 'selected' : ''}
-                    onClick={() => handleFrequencyClick('One Time')}
-                >
-                    One time
-                </button>
-                <button
-                    className={selectedFrequency === 'Monthly' ? 'selected' : ''}
-                    onClick={() => handleFrequencyClick('Monthly')}
-                >
-                    Monthly
-                </button>
+            <div className="container">
+                <div className="donation-text">
+                    <h1>DONATE ONLINE</h1>
+                    <p>
+                        Your donations helps us create lasting change by providing education,
+                        supporting healthcare, empowering communities, and making a real
+                        difference in the lives of those we serve.
+                    </p>
+                </div>
+                <p>Frequency</p>
+                <div className="frequency-buttons">
+                    <button
+                        className={selectedFrequency === 'One Time' ? 'selected' : ''}
+                        onClick={() => handleFrequencyClick('One Time')}
+                    >
+                        One time
+                    </button>
+                    <button
+                        className={selectedFrequency === 'Monthly' ? 'selected' : ''}
+                        onClick={() => handleFrequencyClick('Monthly')}
+                    >
+                        Monthly
+                    </button>
+                </div>
+                <p>Amount</p>
+                <div className="amount-buttons">
+                    <button
+                        className={selectedAmount === '10' ? 'selected' : ''}
+                        onClick={() => handleAmountClick('10')}
+                    >
+                        $10.00
+                    </button>
+                    <button
+                        className={selectedAmount === '50' ? 'selected' : ''}
+                        onClick={() => handleAmountClick('50')}
+                    >
+                        $50.00
+                    </button>
+                    <button
+                        className={selectedAmount === '100' ? 'selected' : ''}
+                        onClick={() => handleAmountClick('100')}
+                    >
+                        $100.00
+                    </button>
+                    <button
+                        className={selectedAmount === '200' ? 'selected' : ''}
+                        onClick={() => handleAmountClick('200')}
+                    >
+                        $200.00
+                    </button>
+                    {/*<button*/}
+                    {/*    className={selectedAmount === 'custom' ? 'selected' : ''}*/}
+                    {/*    onClick={() => handleAmountClick('custom')}*/}
+                    {/*>*/}
+                    {/*    Enter Amount*/}
+                    {/*</button>*/}
+                    <input name="myInput" className={selectedAmount === 'custom' ? 'selected' : ''}
+                           placeholder="Enter Amount" onInput={() => handleAmountClick('custom')}/>
+                </div>
+                <div>
+                    <button className="donate-button">Donate</button>
+                </div>
             </div>
-            <p>Amount</p>
-            <div className="amount-buttons">
-                <button
-                    className={selectedAmount === '10' ? 'selected' : ''}
-                    onClick={() => handleAmountClick('10')}
-                >
-                    $10.00
-                </button>
-                <button
-                    className={selectedAmount === '50' ? 'selected' : ''}
-                    onClick={() => handleAmountClick('50')}
-                >
-                    $50.00
-                </button>
-                <button
-                    className={selectedAmount === '100' ? 'selected' : ''}
-                    onClick={() => handleAmountClick('100')}
-                >
-                    $100.00
-                </button>
-                <button
-                    className={selectedAmount === '200' ? 'selected' : ''}
-                    onClick={() => handleAmountClick('200')}
-                >
-                    $200.00
-                </button>
-                {/*<button*/}
-                {/*    className={selectedAmount === 'custom' ? 'selected' : ''}*/}
-                {/*    onClick={() => handleAmountClick('custom')}*/}
-                {/*>*/}
-                {/*    Enter Amount*/}
-                {/*</button>*/}
-                <input name="myInput" className={selectedAmount === 'custom' ? 'selected' : ''}
-                       placeholder="Enter Amount" onInput={() => handleAmountClick('custom')}/>
-            </div>
-            <div>
-            <button className="donate-button">Donate</button>
-            </div>
-        </div>
+        </>
+
     );
 };
 
